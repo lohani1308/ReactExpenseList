b443ddbbf05ba11c1506c795361b3f4f394c9135
Abhishek Lohani <alohani1308@gmail.com>
2022-06-27 16:23:24 +0530

    adding codebase

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import ExpenseItems from './ExpenseItems';
+import Card from '../UI/Card';
+import ExpensesFilter from "./ExpenseFilter";
+import './Expenses.css';
+
+const Expenses = (props) => {
+   const [filteredYear, setFilteredYear] = useState('2020');
+
+   const filterChangeHandler = (selectedYear) => {
+     setFilteredYear(selectedYear);
+   };
+
+   const filterExpenses=props.items.filter(expense=>{
+     return expense.date.getFullYear().toString()==filteredYear;
+   })
+
+   //console.log(props.items[0].date.getFullYear());
+
+  return (
+    <div>
+      <Card className='expenses'>
+         <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        /> 
+        {/*other than this we can store value in variable og error message ad if length >0 we can update that variable with mapped array*/}
+        {filterExpenses.length===0 ? (<p className="error_msg">No Expenses Found</p>) 
+        :
+        (filterExpenses.map((expense) => (
+          <ExpenseItems
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ) ))}
+
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
